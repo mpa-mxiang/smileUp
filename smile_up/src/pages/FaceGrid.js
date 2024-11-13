9c54b7ea1c26dd09addf607c084a29da558da9f1
Jiachun Xiang <jcxiang1230@gmail.com>
2024-11-12 19:29:23 -0500

    chore: improve shuffle method for pics

--- smile_up/src/pages/FaceGrid.js
+++ smile_up/src/pages/FaceGrid.js
@@ -1,28 +1,29 @@
 import React, { useMemo } from 'react';
 import "./FaceGrid.css";
-import Img1 from "../imgs/pic1.jpg";
-import Img2 from "../imgs/pic2.jpg";
-import Img3 from "../imgs/pic3.jpg";
-import Img4 from "../imgs/pic4.jpg";
-import Img5 from "../imgs/pic5.jpg";
-import Img6 from "../imgs/pic6.jpg";
-import Img7 from "../imgs/pic7.jpg";
-import Img8 from "../imgs/pic8.jpg";
-import Img9 from "../imgs/pic9.jpg";
+import FaceStorage from '../FaceStorage';
 
 function FaceGrid() {
-    const images = [Img1, Img2, Img3, Img4, Img5, Img6, Img7, Img8, Img9];
+    const shuffledImages = useMemo(() => {
+        const NUM_NEUTRAL_FACES = 6;
+        const NUM_SMILING_FACES = 1;
 
-    const shuffledImages = useMemo(() => images
-        .map(value => ({ value, sort: Math.random() }))
-        .sort((a, b) => a.sort - b.sort)
-        .map(({ value }) => value), [images]);
+        const shuffledNeutralFaces = FaceStorage.neutral.sort(() => 0.5 - Math.random());
+        const chosenNeutralFaces = shuffledNeutralFaces.slice(0, NUM_NEUTRAL_FACES); 
+
+        const shuffledSmilingFaces = FaceStorage.smiling.sort(() => 0.5 - Math.random());
+        const chosenSmilingFaces = shuffledSmilingFaces.slice(0, NUM_SMILING_FACES);
+
+        return [...chosenNeutralFaces, ...chosenSmilingFaces]
+            .map(value => ({ value, sort: Math.random() }))
+            .sort((a, b) => a.sort - b.sort)
+            .map(({ value }) => value);
+    }, [FaceStorage.smiling, FaceStorage.neutral]);
 
     return (
         <div className='grid grid-cols-3 w-full'>
             {shuffledImages.map((img, index) => (
                 <div key={index}>
-                    <img src={img} alt={`face-${index}`} />
+                    <img src={img.image} alt={`face-${index}`} />
                 </div>
             ))}
         </div>
