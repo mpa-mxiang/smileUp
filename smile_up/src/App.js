638acaf83a1eb5a1d84746aa816644cf2d53464c
Jiachun Xiang <jcxiang1230@gmail.com>
2024-11-23 23:55:18 -0500

    chore: create navbar

--- smile_up/src/App.js
+++ smile_up/src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import Timer from '../src/components/Timer';
 import FaceGrid from './pages/FaceGrid';
+import Navbar from './components/Navbar';
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1 className="text-3xl font-bold text-red-400">Smile Up</h1>
       <Timer initialSeconds={0} />
       <FaceGrid />
---
+++ smile_up/src/components/Navbar.js
@@ -0,0 +1,28 @@
+import {FaBars, FaTimes} from "react-icons/fa";
+import { useRef } from "react";
+import './Navbar.css';
+function Navbar() {
+    const navRef = useRef();
+    const showNavbar = () => {
+        navRef.current.classList.toggle("responsive_nav");
+    }
+    return(
+        <header>
+            <h1>Logo</h1>
+            <nav ref={navRef}>
+                <a href="/#">About</a>
+                <a href="/#">Features</a>
+                <a href="/#">FAQs</a>
+                <a href="/#">Testimonials</a>
+                <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+                    <FaTimes />
+                </button>
+            </nav>
+            <button className="nav-btn" onClick={showNavbar}>
+                    <FaBars />
+                </button>
+        </header>
+    );
+}
+
+export default Navbar;
